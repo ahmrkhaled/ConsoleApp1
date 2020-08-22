3d8228ed2647a9e910ff85d3eab787fc393d33bf
Ahmed Khaled <ahmkhaled@hotmail.com>
2020-08-22 00:13:58 -0400

    hello

---
+++ Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Course
+    {
+
+        public string GetSomething(string one,
+                                   string two,
+                                   string three,
+                                   string four,
+                                   string five) {
+
+            return "tets";
+        }
+    }
+}
